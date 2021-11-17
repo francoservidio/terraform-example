148c2c8e655510792c1b2c548e3792dda768f6ee
fservidioo <fservidioo@gmail.com>
2021-11-16 22:33:42 -0300

    new variable and public_ip output

--- main.tf
+++ main.tf
@@ -11,6 +11,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for http requests"
+  type = number
+  default = 80
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-04ad2567c9e3d7893"
   instance_type = "t2.micro"
@@ -33,9 +39,9 @@ resource "aws_security_group" "instance" {
   name = "terraform example-instance"
 
   ingress {
-    from_port = 80
+    from_port = var.server_port
     protocol  = "tcp"
-    to_port   = 80
+    to_port   = var.server_port
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
@@ -50,6 +56,12 @@ resource "aws_security_group" "instance" {
     to_port   = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+
 }
 
 
